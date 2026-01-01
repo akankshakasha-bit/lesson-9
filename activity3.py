9b963f3428ccf4227d0525d2c08b30fa1e917823
akankshakasha-bit <akankshakasha@gmail.com>
2026-01-01 20:37:58 +1100

    initial commit

---
+++ activity3.py
@@ -0,0 +1,11 @@
+mean1=20
+wrong_num=34
+correct_num=44
+total_num=50
+sum=mean1*total_num# mean=sum/total_num
+print("the sum of total_num", sum)
+sum2=sum-(wrong_num-correct_num)
+print(sum2)
+
+mean2=sum2/total_num
+print(mean2)
