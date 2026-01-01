1271cdcf2e1db2b5ddba040d41a148c15c800080
akankshakasha-bit <akankshakasha@gmail.com>
2026-01-01 20:21:09 +1100

    initial commit

---
+++ activity1.py
@@ -0,0 +1,13 @@
+a=1
+b=2
+c=3
+d=4
+e=(c-a)*d/b
+print("value of (c-a)*d/b is", e)
+
+name="emma"
+age=18
+if name == "emma"or name == "john" and age <= 12:
+    print("hello! welcome!")
+else:
+    print("goodbye")
